b1ebc0865fef413e34bd69f01416deb8f2506a5c
Yevhenii <eugeniusz.kolesnyk@gmail.com>
2021-03-08 11:02:36 +0200

    - added search on track list page

--- src/pages/tracks/index.tsx
+++ src/pages/tracks/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { PageTemplate } from '@layouts/PageTemplate'
-import { Box, Button, Card, Grid } from '@material-ui/core'
+import { Box, Button, Card, Grid, TextField } from '@material-ui/core'
 import { useRouter } from 'next/router'
 import { TrackList } from '@components/TrackList'
 import { useTypedSelector } from '@hooks/useTypedSelector'
 import { NextThunkDispatch, wrapper } from '@store/store'
-import { fetchTracks } from '@store/operations/tracks.operations'
+import { fetchTracks, searchTracks } from '@store/operations/tracks.operations'
+import { useDispatch } from 'react-redux'
 
 interface TracksPage {}
 
 const TracksPage: React.FC<TracksPage> = (): React.ReactElement => {
     const router = useRouter()
+    const dispatch = useDispatch() as NextThunkDispatch
+
     const { items, error } = useTypedSelector((state) => state.tracks)
+    const [query, setQuery] = useState<string>('')
+    const [timer, setTimer] = useState(null)
+
+    const search = (e: ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value)
+        if (timer) {
+            clearTimeout(timer)
+        }
+        setTimer(
+            setTimeout(async () => {
+                await dispatch(await searchTracks(e.target.value))
+            }, 500)
+        )
+    }
 
     if (error) {
         return (
@@ -31,6 +48,7 @@ const TracksPage: React.FC<TracksPage> = (): React.ReactElement => {
                             <Button onClick={() => router.push('/tracks/create')}>Upload</Button>
                         </Grid>
                     </Box>
+                    <TextField fullWidth value={query} onChange={search} />
                     <TrackList tracks={items} />
                 </Card>
             </Grid>
--- src/store/operations/tracks.operations.ts
+++ src/store/operations/tracks.operations.ts
@@ -14,4 +14,15 @@ export const fetchTracks = () => {
     }
 }
 
+export const searchTracks = (query: string) => {
+    return async (dispatch: Dispatch<TracksActions>) => {
+        try {
+            const response = await axios.get<FetchTracksResponse>(`http://localhost:5000/tracks/search?query=${query}`)
+            dispatch(tracksActions.fetchTracks(response.data))
+        } catch (e) {
+            dispatch(tracksActions.fetchTracksError('Some error occurred with fetching tracks'))
+        }
+    }
+}
+
 type FetchTracksResponse = Track[]
